db04b0675a5559708e0f51120af8ff4771163972
南天 <1192487709@qq.com>
2024-12-17 23:38:29 +0800

    更新电影搜索逻辑

--- html/AA-代码最后更新时间.js
+++ html/AA-代码最后更新时间.js
@@ -1,3 +1,3 @@
-var codeAA = '<div class="item comment">代码更新 2024年12月16日 16点41分</div>'; document.getElementById('codeAA').innerHTML = codeAA;
+var codeAA = '<div class="item comment">代码更新 2024年12月16日 23点37分</div>'; document.getElementById('codeAA').innerHTML = codeAA;
 
 /* html文件夹于2024.5.11,最后一次更新 */
